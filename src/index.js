b1db15df17f5e5fd5e7fdcd66d9239ce2af14414
AndrewMik <andrew.mikitchuk@gmail.com>
2023-01-16 15:19:21 +0400

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodedExpression = "";
+    const result = expr.match(/.{1,10}/g);
+    for (let i = 0; i < result.length; i++) {
+        if(result[i] === "**********"){
+            decodedExpression += " ";
+        } else {
+            let decodedSymbol = result[i].replace(/10/g, ".");
+            decodedSymbol = decodedSymbol.replace(/11/g, "-");
+            decodedSymbol = decodedSymbol.replace(/0/g, "");
+            decodedExpression += MORSE_TABLE[decodedSymbol];
+        }
+    }
+    return decodedExpression;
 }
 
 module.exports = {
